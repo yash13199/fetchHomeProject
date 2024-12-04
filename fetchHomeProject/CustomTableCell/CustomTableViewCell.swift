2a75e71e361286c3a0b8d42ea88f92a5013fc71b
yash13199 <52669742+yash13199@users.noreply.github.com>
2024-12-03 16:43:16 -0800

    all task done

---
+++ fetchHomeProject/CustomTableCell/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  fetchHomeProject
+//
+//  Created by Sohagia Yash on 12/3/24.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ fetchHomeProject/functions.swift
@@ -0,0 +1,8 @@
+//
+//  functions.swift
+//  fetchHomeProject
+//
+//  Created by Sohagia Yash on 12/3/24.
+//
+
+import Foundation
